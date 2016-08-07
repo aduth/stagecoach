7e915c5cf4001845b002d9953085fa2ff63d9382
Andrew Duthie <aduthie7@gmail.com>
2016-08-06 20:48:17 -0400

    Add Sidebar component

--- src/layout/app/index.js
+++ src/layout/app/index.js
@@ -3,9 +3,15 @@
  */
 import { h } from 'preact';
 
+/**
+ * Internal dependencies
+ */
+import Sidebar from 'layout/sidebar';
+
 export default function App() {
 	return (
 		<main className="app">
+			<Sidebar />
 			App
 		</main>
 	);
---
+++ src/layout/sidebar/index.js
@@ -0,0 +1,12 @@
+/**
+ * External dependencies
+ */
+import { h } from 'preact';
+
+export default function Sidebar() {
+	return (
+		<nav className="sidebar">
+			Sidebar
+		</nav>
+	);
+}
