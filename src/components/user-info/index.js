c18879545fcb11e5abfc4c4adf8ac83a9ace7803
Andrew Duthie <aduthie7@gmail.com>
2016-08-06 20:55:16 -0400

    Add UserInfo component

---
+++ src/components/user-info/index.js
@@ -0,0 +1,12 @@
+/**
+ * External dependencies
+ */
+import { h } from 'preact';
+
+export default function UserInfo() {
+	return (
+		<div className="user-info">
+			User Info
+		</div>
+	);
+}
--- src/layout/sidebar/index.js
+++ src/layout/sidebar/index.js
@@ -3,9 +3,15 @@
  */
 import { h } from 'preact';
 
+/**
+ * Internal dependencies
+ */
+import UserInfo from 'components/user-info';
+
 export default function Sidebar() {
 	return (
 		<nav className="sidebar">
+			<UserInfo />
 			Sidebar
 		</nav>
 	);
