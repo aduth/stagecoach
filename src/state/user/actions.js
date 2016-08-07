1fbec06050339b5b2e0f88471bc35f7f79998a29
Andrew Duthie <aduthie7@gmail.com>
2016-08-06 21:37:11 -0400

    Add user state handlers for requesting current user

--- src/state/user/actions.js
+++ src/state/user/actions.js
@@ -1,11 +1,45 @@
 /**
  * Internal dependencies
  */
-import { USER_TOKEN_SET } from 'state/action-types';
+import { requestWithToken } from 'lib/github-request';
+import {
+	USER_RECEIVE,
+	USER_REQUEST,
+	USER_REQUEST_FAILURE,
+	USER_REQUEST_SUCCESS,
+	USER_TOKEN_SET
+} from 'state/action-types';
 
 export function setUserToken( token ) {
 	return {
 		type: USER_TOKEN_SET,
 		token
+	};
+}
+
+export function receiveUser( user ) {
+	return {
+		type: USER_RECEIVE,
+		user
+	};
+}
+
+export function requestUser( token ) {
+	return async ( dispatch ) => {
+		dispatch( {
+			type: USER_REQUEST
+		} );
+
+		try {
+			const response = await requestWithToken( '/user', token );
+			const user = await response.json();
+			dispatch( receiveUser( user ) );
+			dispatch( { type: USER_REQUEST_SUCCESS } );
+		} catch ( error ) {
+			dispatch( {
+				type: USER_REQUEST_FAILURE,
+				error
+			} );
+		}
 	};
 }
--- src/state/user/reducer.js
+++ src/state/user/reducer.js
@@ -6,7 +6,33 @@ import { combineReducers } from 'redux';
 /**
  * Internal dependencies
  */
-import { USER_TOKEN_SET } from 'state/action-types';
+import {
+	USER_RECEIVE,
+	USER_REQUEST,
+	USER_REQUEST_FAILURE,
+	USER_REQUEST_SUCCESS,
+	USER_TOKEN_SET
+} from 'state/action-types';
+
+export function requesting( state = false, action ) {
+	switch ( action.type ) {
+		case USER_REQUEST:
+		case USER_REQUEST_FAILURE:
+		case USER_REQUEST_SUCCESS:
+			return USER_REQUEST === action.type;
+	}
+
+	return state;
+}
+
+export function user( state = null, action ) {
+	switch ( action.type ) {
+		case USER_RECEIVE:
+			return action.user;
+	}
+
+	return state;
+}
 
 export function token( state = null, action ) {
 	switch ( action.type ) {
@@ -18,5 +44,7 @@ export function token( state = null, action ) {
 }
 
 export default combineReducers( {
+	requesting,
+	user,
 	token
 } );
