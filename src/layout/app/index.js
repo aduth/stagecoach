807e1d55c0c3fb7b4d305fdccbc1a4e78c4a9c4e
Andrew Duthie <aduthie7@gmail.com>
2016-08-06 20:51:22 -0400

    Add Inbox component

--- src/layout/app/index.js
+++ src/layout/app/index.js
@@ -7,11 +7,13 @@ import { h } from 'preact';
  * Internal dependencies
  */
 import Sidebar from 'layout/sidebar';
+import Inbox from 'layout/inbox';
 
 export default function App() {
 	return (
 		<main className="app">
 			<Sidebar />
+			<Inbox />
 			App
 		</main>
 	);
---
+++ src/layout/inbox/index.js
@@ -0,0 +1,12 @@
+/**
+ * External dependencies
+ */
+import { h } from 'preact';
+
+export default function Inbox() {
+	return (
+		<div className="inbox">
+			Inbox
+		</div>
+	);
+}
